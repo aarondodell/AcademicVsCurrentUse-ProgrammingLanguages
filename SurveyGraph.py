5b5178c1df32c7ef6155f024caa16b0b269bea9a
Aaron D <t-alternative@live.com>
2018-05-26 18:01:36 -0700

    initial Language Labels
    addition of Language Labels, resizing of logos and labels still needed

--- SurveyGraph.py
+++ SurveyGraph.py
@@ -1,12 +1,18 @@
 import os
 import pandas as pd
+import numpy as np
+import itertools as it
 from pytictoc import TicToc
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image, ImageOps, ImageFont, ImageDraw
 from plotnine import *
 
 # TIC
 tt = TicToc()
 tt.tic()
+
+"""
+STEP 1: reading and processing Survey and Language info
+"""
 
 # reading raw_survey
 os.chdir('.\Sources')
@@ -34,6 +40,76 @@ colors_ref.index = colors_index
 # finding hex codes sorted
 colornames_sortedlst = sorted(main_survey_df['Language'].unique())
 colorhex_sortedlst = [colors_ref.at[cname, 'color'] for cname in colornames_sortedlst]
+
+"""
+STEP 2: creating Language Label images
+"""
+for lang_name in colornames_sortedlst:
+	# finding lang color using Ref table
+	lang_color = colors_ref.at[lang_name, 'color']
+	
+	# reading Logo
+	os.chdir('.\LanguageInfo')
+	if lang_name == 'C++':
+		lang_logo = Image.open('Cpp.png')
+	elif lang_name == 'C#':
+		lang_logo = Image.open('Csharp.png')
+	else:
+		lang_logo = Image.open(lang_name+'.png')
+	
+	os.chdir('..')
+	
+	print(lang_name, lang_logo.size)
+	
+	# creating white background
+	background_img = Image.new('RGBA', (680, 280), 'white')
+	# adding border
+	background_img = ImageOps.expand(background_img, border = 10, fill = lang_color)
+	
+	# resizing LangLogo
+	lang_logo = lang_logo.resize((240,240))
+	
+	# pasting LangLogo onto background, creating full image
+	full_img = background_img.copy()
+	# try as mask
+	try:
+		full_img.paste(lang_logo, (30,30), mask = lang_logo)
+	# if logo is no mask, paste as normal
+	except ValueError:
+		full_img.paste(lang_logo, (30,30))
+	
+	# creating colored font
+	lang_fnt = ImageFont.truetype('ARLRDBD.TTF', 72)
+	# finding label size, given the name
+	lang_labelsize = lang_fnt.getsize(lang_name)
+	# determining location of the Language Label
+	
+	
+	# draw object
+	full_draw = ImageDraw.Draw(full_img)
+	
+	# drawing black outline... anchor is (290, 30)
+	for position_tup in (list(it.product(np.arange(287, 294), [18])) +	#top border
+							  list(it.product(np.arange(289, 291), [17])) +	#top border extra
+							  list(it.product([292], np.arange(18, 23))) +	#right border
+							  list(it.product([293], np.arange(19, 22))) +	#right border extra
+							  list(it.product(np.arange(287,294), [22])) +	#bottom border
+							  list(it.product(np.arange(289,291), [23])) +	#bottom border extra
+							  list(it.product([288], np.arange(18,23))) +	#left border
+							  list(it.product([287], np.arange(19,22)))):	#left border extra
+		full_draw.text(position_tup, lang_name, fill = 'black', font = lang_fnt)
+	
+	# drawing colored center
+	full_draw.text((290,20), lang_name, fill = lang_color, font = lang_fnt)
+	
+	# saving image
+	os.chdir('.\LanguageLabels')
+	full_img.save(lang_name+'.png')
+	os.chdir('..')
+
+"""
+STEP 3: ggplotting
+"""
 
 # initial Parallel df
 parallel_df = pd.melt(main_survey_df, id_vars = 'Language', value_vars = ['Q1_Academic_Perc', 'Q2_Current_Perc'],
